86f0eb79de8eca217c3069f436312a663de2aa6c
Vinícius Santiago <viniciussantiagosantos19@gmail.com>
2021-03-31 21:30:25 -0300

    Calculadora e sua Funções

---
+++ js/funcoes.js
@@ -0,0 +1,9 @@
+let primeiroNumero = parseFloat(prompt("Informe o primeiro número"));
+
+let operacao = parseInt(prompt("Escolha 1 para soma, 2 para subtração, 3 para multiplicação e 4 para divisão"));
+
+let segundoNumero = parseFloat(prompt("Informe o segundo númeor"));
+
+let resultado = primeiroNumero + segundoNumero;
+
+document.write("<h2>" + primeiroNumero + "+" + segundoNumero + "=" + resultado + "</h2>") 
